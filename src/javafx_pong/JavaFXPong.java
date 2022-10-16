af534fe36b4126b94e3fbb950d966f21bab5c82d
Y Nasr <yoyo.monem22@gmail.com>
2022-10-16 19:53:18 +0200

    ???

--- src/javafx_pong/JavaFXPong.java
+++ src/javafx_pong/JavaFXPong.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-public class PongGame {
+public class JavaFXPong {
 	public static void main(String[] args) {
 		GameFrame frame = new GameFrame();
 	}
